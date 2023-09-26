eeaf41ca1be327fb9c90b29179de8c8e19c31bd6
Cedric Developer LFCS <cedric.deveops@gmail.com>
2023-09-26 15:55:39 +0000

    All files

---
+++ archive-dnf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if test -f /tmp/archive.tar.gz; then
+          mv /tmp/archive.tar.gz /tmp/archive.tar.gz.old
+	tar acf /tmp/archive.tar.gz /etc/dnf
+else
+	tar acf /tmp/archive.tar.gz /etc/dnf
+fi
